25486ff32cebfc708d359b6490af9429e51eb332
gmiller77 <43762856+gmiller77@users.noreply.github.com>
2023-05-01 22:12:25 +0200

    Integr. test refactored (to new classes)

--- hr/src/main/java/hu/webuni/hr/greg77/service/CompanyService.java
+++ hr/src/main/java/hu/webuni/hr/greg77/service/CompanyService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -107,6 +108,7 @@ public class CompanyService {
 	}
 	
 	@Transactional
+	@Modifying
 	public void setMinSalary(String positionName, long companyId, int minSalary) {
 		positionDetailsByCompanyRepository.findByPositionNameAndCompanyId(positionName, companyId)
 		.forEach(pd -> {
--- hr/src/test/java/hu/webuni/hr/greg77/web/EmployeeControllerIT.java
+++ hr/src/test/java/hu/webuni/hr/greg77/web/EmployeeControllerIT.java
@@ -3,17 +3,23 @@ package hu.webuni.hr.greg77.web;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
+import org.assertj.core.data.TemporalUnitWithinOffset;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.test.web.reactive.server.WebTestClient.ResponseSpec;
 
 import hu.webuni.hr.greg77.dto.EmployeeDto;
 
+@AutoConfigureTestDatabase
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class EmployeeControllerIT {
 
@@ -26,13 +32,18 @@ public class EmployeeControllerIT {
 	void testThatCreatedEmployeeIsListed() throws Exception {
 		List<EmployeeDto> employeesBefore = getAllEmployees();
 
-		EmployeeDto newEmployeeDto = new EmployeeDto(10L, "Thomas Test", null, 12_345,
+		EmployeeDto newEmployeeDto = new EmployeeDto(0L, "Thomas Test", "tesztelő", 12_345,
 				LocalDateTime.now().minusMonths(100));
 
-		createEmployee(newEmployeeDto);
+		saveEmployee(newEmployeeDto)
+			.expectStatus()
+			.isOk();
+		
+//		createEmployee(newEmployeeDto);
 
 		List<EmployeeDto> employeesAfter = getAllEmployees();
 
+		/*
 		assertThat(employeesAfter
 			.subList(0, employeesBefore.size()))
 			.usingRecursiveFieldByFieldElementComparator()
@@ -41,6 +52,16 @@ public class EmployeeControllerIT {
 		assertThat(employeesAfter.get(employeesAfter.size() - 1))
 			.usingRecursiveComparison()
 			.isEqualTo(newEmployeeDto);
+		*/
+		
+		assertThat(employeesAfter.size()).isEqualTo(employeesBefore.size() + 1);
+		assertThat(employeesAfter.get(employeesAfter.size()-1))
+			.usingRecursiveComparison()
+			.ignoringFields("id","startDate")
+			.isEqualTo(newEmployeeDto);
+		assertThat(employeesAfter.get(employeesAfter.size()-1)
+				.getStartDate())
+				.isCloseTo(newEmployeeDto.getStartDate(), new TemporalUnitWithinOffset(1,ChronoUnit.SECONDS));
 	}
 
 	@Test
@@ -69,7 +90,7 @@ public class EmployeeControllerIT {
 	void testThatModifiedEmployeeHasUpdatedData() throws Exception {
 		List<EmployeeDto> employeesBefore = getAllEmployees();
 
-		EmployeeDto newEmployeeDto = new EmployeeDto(1L, "Thomas Test", null, 12_345,
+		EmployeeDto newEmployeeDto = new EmployeeDto(1L, "Thomas Test", "tesztelő", 12_345,
 				LocalDateTime.now().minusMonths(100));
 
 		updateEmployee(newEmployeeDto);
@@ -79,7 +100,13 @@ public class EmployeeControllerIT {
 		int index = findEmployeeIndex(employeesAfter, newEmployeeDto.getId());
 		
 		assertThat(employeesAfter.size()).isEqualTo(employeesBefore.size());
-		assertThat(employeesAfter.get(index)).usingRecursiveComparison().isEqualTo(newEmployeeDto);
+		assertThat(employeesAfter.get(index))
+			.usingRecursiveComparison()
+			.ignoringFields("startDate")
+			.isEqualTo(newEmployeeDto);
+		
+		assertThat(employeesAfter.get(index).getStartDate())
+				.isCloseTo(newEmployeeDto.getStartDate(), new TemporalUnitWithinOffset(1,ChronoUnit.SECONDS));
 		
 		/*
 		System.out.println("EmployeesBefore list:");
@@ -134,6 +161,7 @@ public class EmployeeControllerIT {
 			.isOk();
 	}
 	
+
 	private void createInvalidEmployee(EmployeeDto newEmployeeDto) {
 		webTestClient
 			.put()
@@ -142,16 +170,6 @@ public class EmployeeControllerIT {
 			.exchange()
 			.expectStatus()
 			.isBadRequest();		
-	}
-	
-	private void updateEmployee(EmployeeDto newEmployeeDto) {
-		webTestClient
-			.put()
-			.uri(BASE_URI + "/" + newEmployeeDto.getId())
-			.bodyValue(newEmployeeDto)
-			.exchange()
-			.expectStatus()
-			.isOk();
 	}
 	
 	private void updateInvalidEmployee(EmployeeDto newEmployeeDto) {
@@ -164,10 +182,36 @@ public class EmployeeControllerIT {
 			.isBadRequest();
 	}
 
+	private void updateEmployee(EmployeeDto newEmployeeDto) {
+		webTestClient
+			.put()
+			.uri(BASE_URI + "/" + newEmployeeDto.getId())
+			.bodyValue(newEmployeeDto)
+			.exchange()
+			.expectStatus()
+			.isOk();
+	}
+	
+	private ResponseSpec saveEmployee(EmployeeDto newEmployee) {
+		return webTestClient
+				.post()
+				.uri(BASE_URI)
+				.bodyValue(newEmployee)
+				.exchange();
+	}
+	
 	private List<EmployeeDto> getAllEmployees() {
-		List<EmployeeDto> responseList = webTestClient.get().uri(BASE_URI).exchange().expectStatus().isOk()
-				.expectBodyList(EmployeeDto.class).returnResult().getResponseBody();
-		Collections.sort(responseList, (a1, a2) -> Long.compare(a1.getId(), a2.getId()));
+		List<EmployeeDto> responseList = webTestClient
+				.get()
+				.uri(BASE_URI)
+				.exchange()
+				.expectStatus()
+				.isOk()
+				.expectBodyList(EmployeeDto.class)
+				.returnResult()
+				.getResponseBody();
+//		Collections.sort(responseList, (a1, a2) -> Long.compare(a1.getId(), a2.getId()));
+		Collections.sort(responseList, Comparator.comparing(EmployeeDto::getId));
 		return responseList;
 	}
 }
