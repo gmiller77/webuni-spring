3360d799ace993b5d80ee7647c4c05ddf1719283
gmiller77 <43762856+gmiller77@users.noreply.github.com>
2023-05-07 11:32:55 +0200

    getPayRaisePercent() method changed in SalaryController.java
    from @PostMapping to @GetMapping

--- hr/src/main/java/hu/webuni/hr/greg77/web/SalaryController.java
+++ hr/src/main/java/hu/webuni/hr/greg77/web/SalaryController.java
@@ -1,8 +1,8 @@
 package hu.webuni.hr.greg77.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,7 +22,7 @@ public class SalaryController {
 	@Autowired
 	CompanyService companyService;
 
-	@PostMapping("/payRaisePercent")
+	@GetMapping("/payRaisePercent")
 	public int getPayRaisePercent(@RequestBody Employee employee) {
 		return employeeService.getPayRaisePercent(employee);
 	}
